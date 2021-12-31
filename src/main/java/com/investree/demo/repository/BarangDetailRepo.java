9541cbe318ba5a70ce862ae2fedb011612d5da4a
riki aldi pari <rikialdipari@gmail.com>
2021-12-31 18:56:49 +0700

    update

--- src/main/java/com/investree/demo/repository/BarangDetailRepo.java
+++
@@ -1,9 +0,0 @@
-package com.investree.demo.repository;
-import com.investree.demo.model.BarangDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BarangDetailRepo extends JpaRepository<BarangDetail, Long> {
-
-}
--- src/main/java/com/investree/demo/view/DosenInterface.java
+++
@@ -1,7 +0,0 @@
-package com.investree.demo.view;
-
-import com.investree.demo.model.Dosen;
-
-public interface DosenInterface {
-    public Dosen insert(Dosen obj);
-}
