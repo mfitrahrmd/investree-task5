7e21c6ddb008b55a29c135d9b2f13eb3b7da4bf0
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-03-15 23:58:45 +0700

    define transaksi service

---
+++ src/main/java/com/investree/demo/view/TransaksiService.java
@@ -0,0 +1,12 @@
+package com.investree.demo.view;
+
+import com.investree.demo.model.Transaksi;
+
+import java.util.Map;
+
+public interface TransaksiService {
+    Map save(Transaksi transaksi);
+    Map updateStatus(Transaksi transaksi);
+    Map list(Integer page, Integer size, String status);
+
+}
