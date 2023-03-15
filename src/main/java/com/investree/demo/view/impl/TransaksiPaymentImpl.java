c20e9f460ccddf600748f655b905911ea9fd60c3
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-03-16 00:07:58 +0700

    implement list transaksi service

--- src/main/java/com/investree/demo/view/impl/TransaksiPaymentImpl.java
+++ src/main/java/com/investree/demo/view/impl/TransaksiPaymentImpl.java
@@ -4,6 +4,8 @@ import com.investree.demo.model.Transaksi;
 import com.investree.demo.repository.TransaksiRepository;
 import com.investree.demo.view.TransaksiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -36,7 +38,13 @@ public class TransaksiPaymentImpl implements TransaksiService {
     }
 
     public Map list(Integer page, Integer size, String status) {
-        return null;
-        // TODO : implement me
+        Pageable pageable = size == null || size < 1 ? null : Pageable.ofSize(size).withPage(page == null ? 0 : page); // check size for each page, list all data if it less than 1
+        status = status == null ? "" : status;
+
+        Page<Transaksi> transaksiPage = transaksiRepository.findAllByStatusContainsWithPagination(status, pageable);
+
+        return Map.of(
+                "transaksiPage", transaksiPage
+        );
     }
 }
